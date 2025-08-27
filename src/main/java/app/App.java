6b0a53dff39e3e4bf251d7567ca9118b9f5ebeb5
SantiagoBorgna <santiborgna5@gmail.com>
2025-08-27 19:02:20 -0300

    DB connection

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,6 +1,30 @@
+package app;
+
+import app.dao.DBConnection;
+
+import java.sql.Connection;
+
 public class App {
 
-    public static void main(String[] args) {
+    //static String url = "jdbc:mysql://127.0.0.1:3306/arcadb";
 
+    //static String usuario = "santi";
+
+    //static String contrasena = "";
+
+    //static Image logo = Toolkit.getDefaultToolkit().getImage(App.class.getResource("/logo arca.png"));
+
+    //static JTable listTable;
+
+    //static DecimalFormat formato1 = new DecimalFormat("#.00");
+
+    public static void main(String[] args) {
+        try (Connection conn = DBConnection.getConnection()) {
+            if (conn != null) {
+                System.out.println("Conexión exitosa a SQLite!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
